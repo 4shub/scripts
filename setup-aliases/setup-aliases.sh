0229c3903d0b43cf667b1ecc9a630f3005711da8
Shubham Naik <shub@shub.club>
2021-02-23 20:26:02 -0800

    feat: reorganize

--- setup-aliases/setup-aliases.sh
+++
@@ -1,9 +0,0 @@
-cp ./aliases ~/.aliases
-
-if [[ $(find ~/.zshrc -type f -print | xargs grep "source ~/.aliases") ]]; then
-    echo "link to alias already created"
-else
-    echo "source ~/.aliases" >> ~/.zshrc
-fi
-
-echo "success"
